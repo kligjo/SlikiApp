29224a29e28199b42b55b96ceb0ff1ef2391be05
Kliment <a@b.c>
2026-07-01 20:00:14 +0200

    fixing gri

---
+++ Sliki.Web/wwwroot/js/gallery.js
@@ -0,0 +1,10 @@
+window.slikiGallery = {
+    downloadFile(url, filename) {
+        const a = document.createElement('a');
+        a.href = url;
+        a.download = filename;
+        document.body.appendChild(a);
+        a.click();
+        document.body.removeChild(a);
+    }
+};
